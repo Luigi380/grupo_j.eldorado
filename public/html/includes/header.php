bbb294a62d7635d47770dda6e1678cfa157c292c
Gustavo de Paula <gustavobonjardim06@gmail.com>
2025-11-17 14:33:15 -0300

    commit

---
+++ public/html/includes/header.php
@@ -0,0 +1,23 @@
+<?php
+// Header reutilizável - inclui áreas de navegação e logo
+// Incluir em páginas PHP com: include __DIR__ . '/includes/header.php';
+?>
+<header>
+  <nav>
+    <ul>
+      <li><a href="#materiais">Rochas Naturais</a></li>
+      <li><a href="">Importadas</a></li>
+    </ul>
+  </nav>
+
+  <div class="logo">
+    <a href="../Home/index.php"><img src="../../img/Logo.png" alt="Logo da Marmoraria" /></a>
+  </div>
+
+  <nav>
+    <ul>
+      <li><a href="#sobre-nos">Sobre nós</a></li>
+      <li><a href="#contato">Contato</a></li>
+    </ul>
+  </nav>
+</header>
